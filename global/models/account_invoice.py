83b58609018cc681b5b6f1bedb2fa7ef687f6aa3
Santi TK <santiago@tkanalytics.es>
2021-07-15 17:02:16 +0200

    Ultimos digitos factura

--- global/models/account_invoice.py
+++ global/models/account_invoice.py
@@ -12,16 +12,17 @@ class AccountMove(models.Model):
     @api.depends("sdd_paying_mandate_id")
     def _compute_ultimos_digitos_iban(self):
         for record in self:
-            if record.sdd_paying_mandate_id is not None:
+            if record.sdd_paying_mandate_id is not None and record.sdd_paying_mandate_id.partner_bank_id is not None:
                 cuenta = record.sdd_paying_mandate_id.partner_bank_id.acc_number
                 salida = ''
-                if cuenta:
-                    longitud = len(cuenta)
-                    if longitud > 4:
-                        for i in range(0, longitud - 4):
-                            salida += '*'
-                        salida += cuenta[-4:]
-                        record.last_digits = salida
+                # record.last_digits = ''
+                # if cuenta:
+                #     longitud = len(cuenta)
+                #     if longitud > 4:
+                #         for i in range(0, longitud - 4):
+                #             salida += '*'
+                #         salida += cuenta[-4:]
+                #         record.last_digits = salida
 
     last_digits = fields.Char(string="Últimos dígitos IBAN Cliente", compute=_compute_ultimos_digitos_iban, store=False)
 
