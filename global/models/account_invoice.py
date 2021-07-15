7dab2a010d4f94ec3c374a1484584d278beadeed
Santi TK <santiago@tkanalytics.es>
2021-07-15 16:49:54 +0200

    Ultimos digitos factura

--- global/models/account_invoice.py
+++ global/models/account_invoice.py
@@ -5,6 +5,7 @@ from odoo import api, fields, models, _
 
 from odoo.exceptions import UserError
 
+
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
@@ -13,14 +14,14 @@ class AccountMove(models.Model):
         for record in self:
             if record.sdd_paying_mandate_id is not None:
                 cuenta = record.sdd_paying_mandate_id.partner_bank_id.acc_number
-                longitud = len(cuenta)
-                if longitud > 4:
-                    for i in range(0, longitud-4):
-                        cuenta[i] = '*'
+                salida = ''
+                if cuenta:
+                    longitud = len(cuenta)
+                    if longitud > 4:
+                        for i in range(0, longitud - 4):
+                            salida += '*'
+                        salida += cuenta[-4:]
+                        record.last_digits = salida
 
-                    record.last_digits = cuenta
-
-    last_digits = fields.Text(string="Últimos dígitos IBAN", computed=_compute_ultimos_digitos_iban, store=False)
-
-
+    last_digits = fields.Char(string="Últimos dígitos IBAN Cliente", compute=_compute_ultimos_digitos_iban, store=False)
 
