3e901971a8b84f30ad164a075c0230f77ecff59c
angelmoya <amoyapardo@gmail.com>
2023-04-19 13:02:43 +0200

    IMP send invoice

---
+++ global/wizard/account_invoice_send.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import api, fields, models, _
+
+
+class AccountInvoiceSend(models.TransientModel):
+    _inherit = 'account.invoice.send'
+
+    def send_and_print_action(self):
+        self.ensure_one()
+        # Send the mails in the correct language by splitting the ids per lang.
+        # This should ideally be fixed in mail_compose_message, so when a fix is made there this whole commit should be reverted.
+        # basically self.body (which could be manually edited) extracts self.template_id,
+        # which is then not translated for each customer.
+        if self.composition_mode == 'mass_mail' and self.template_id:
+            active_ids = self.env.context.get('active_ids', self.res_id)
+            active_records = self.env[self.model].browse(active_ids)
+            for record in active_records:
+                self_record = self.with_context(active_ids=record.ids, lang=record.partner_id.lang)
+                self_record.onchange_template_id()
+                self_record._send_email()
+        else:
+            self._send_email()
+        if self.is_print:
+            return self._print_document()
+        return {'type': 'ir.actions.act_window_close'}
+
