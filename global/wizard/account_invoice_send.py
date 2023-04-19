3a8d77e2c16f737501717444fedacf610fb788bc
angelmoya <amoyapardo@gmail.com>
2023-04-19 13:17:21 +0200

    IMP send invoice - comment

--- global/wizard/account_invoice_send.py
+++ global/wizard/account_invoice_send.py
@@ -18,6 +18,7 @@ class AccountInvoiceSend(models.TransientModel):
             active_records = self.env[self.model].browse(active_ids)
             for record in active_records:
                 self_record = self.with_context(active_ids=record.ids, lang=record.partner_id.lang)
+                self_record.composition_mode = 'comment'
                 self_record.onchange_template_id()
                 self_record.partner_ids = record.partner_id
                 self_record._send_email()
