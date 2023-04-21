5579f6902b1f984013623e0ed2dce721664ed679
angelmoya <amoyapardo@gmail.com>
2023-04-21 13:30:51 +0200

    IMP invoice send

--- global/wizard/account_invoice_send.py
+++ global/wizard/account_invoice_send.py
@@ -32,17 +32,17 @@ class AccountInvoiceSend(models.TransientModel):
                     active_model='account.move',
                     force_email=True
                 )
-                new = self.with_context(ctx).copy({
+                self.with_context(ctx).write({
                     'composition_mode': 'comment',
                     'res_id': record.id,
                     'invoice_ids': [(6, 0, [record.id])],
                     })
-                new.composer_id.send_mail()
-                if new.env.context.get('mark_invoice_as_sent'):
+                self.composer_id.send_mail()
+                if self.env.context.get('mark_invoice_as_sent'):
                     #Salesman send posted invoice, without the right to write
                     #but they should have the right to change this flag
                     self.mapped('invoice_ids').sudo().write({'invoice_sent': True})
-                for inv in new.invoice_ids:
+                for inv in self.invoice_ids:
                     if hasattr(inv, 'attachment_ids') and inv.attachment_ids:
                         inv._message_set_main_attachment_id([(False,att) for att in inv.attachment_ids.ids])
         else:
