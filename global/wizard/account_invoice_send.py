da5f7a57c9bc26ad75d0cad809ba2fb367eb2605
angelmoya <amoyapardo@gmail.com>
2023-04-21 13:27:57 +0200

    IMP invoice send

--- global/wizard/account_invoice_send.py
+++ global/wizard/account_invoice_send.py
@@ -27,18 +27,9 @@ class AccountInvoiceSend(models.TransientModel):
                     lang = lang.code
                 compose_form = self.env.ref('account.account_invoice_send_wizard_form', raise_if_not_found=False)
                 ctx = dict(
-                    default_model='account.move',
-                    default_res_id=record.id,
-                    # For the sake of consistency we need a default_res_model if
-                    # default_res_id is set. Not renaming default_model as it can
-                    # create many side-effects.
-                    default_res_model='account.move',
-                    default_use_template=bool(template),
-                    default_template_id=template and template.id or False,
-                    default_composition_mode='comment',
-                    mark_invoice_as_sent=True,
-                    custom_layout="mail.mail_notification_paynow",
-                    # model_description=self.with_context(lang=lang).type_name,
+                    active_ids=[record.id],
+                    active_id=record.id,
+                    active_model='account.move',
                     force_email=True
                 )
                 new = self.with_context(ctx).copy({
@@ -47,11 +38,11 @@ class AccountInvoiceSend(models.TransientModel):
                     'invoice_ids': [(6, 0, [record.id])],
                     })
                 new.composer_id.send_mail()
-                if self.env.context.get('mark_invoice_as_sent'):
+                if new.env.context.get('mark_invoice_as_sent'):
                     #Salesman send posted invoice, without the right to write
                     #but they should have the right to change this flag
                     self.mapped('invoice_ids').sudo().write({'invoice_sent': True})
-                for inv in self.invoice_ids:
+                for inv in new.invoice_ids:
                     if hasattr(inv, 'attachment_ids') and inv.attachment_ids:
                         inv._message_set_main_attachment_id([(False,att) for att in inv.attachment_ids.ids])
         else:
