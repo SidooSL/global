92dd454cacc6c842c8c4b1ba24199ef005461d36
angelmoya <amoyapardo@gmail.com>
2023-04-21 13:11:53 +0200

    IMP invoice send

--- global/wizard/account_invoice_send.py
+++ global/wizard/account_invoice_send.py
@@ -2,6 +2,8 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import api, fields, models, _
+from odoo.tools.misc import formatLang, format_date, get_lang
+
 
 
 class AccountInvoiceSend(models.TransientModel):
@@ -17,23 +19,34 @@ class AccountInvoiceSend(models.TransientModel):
             active_ids = self.env.context.get('active_ids', self.res_id)
             active_records = self.env[self.model].browse(active_ids)
             for record in active_records:
-                lang = record.partner_id.lang
-                self_record = self.with_context(
-                    active_ids=record.ids,
-                    active_id=record.id,
-                    lang=lang,
+                template = self.env.ref('account.email_template_edi_invoice', raise_if_not_found=False)
+                lang = get_lang(self.env)
+                if template and template.lang:
+                    lang = template._render_template(template.lang, 'account.move', record.id)
+                else:
+                    lang = lang.code
+                compose_form = self.env.ref('account.account_invoice_send_wizard_form', raise_if_not_found=False)
+                ctx = dict(
+                    default_model='account.move',
                     default_res_id=record.id,
+                    # For the sake of consistency we need a default_res_model if
+                    # default_res_id is set. Not renaming default_model as it can
+                    # create many side-effects.
                     default_res_model='account.move',
+                    default_use_template=bool(template),
+                    default_template_id=template and template.id or False,
+                    default_composition_mode='comment',
                     mark_invoice_as_sent=True,
                     custom_layout="mail.mail_notification_paynow",
-                    force_email=True)
-                self_record.write({
-                    'composition_mode': 'comment',
-                    'template_id': self.template_id.id,
-                    })
-                self_record.onchange_template_id()
-                self_record.partner_ids = record.partner_id
-                self_record._send_email()
+                    model_description=self.with_context(lang=lang).type_name,
+                    force_email=True
+                )
+                new = self.env['account.invoice.send'].with_context(ctx).create({
+                    'invoice_ids': [(6, 0, [record.id])],
+                    'is_email': self.is_email,
+                    'is_print': self.is_print,
+                })
+                new._send_email()
         else:
             self._send_email()
         if self.is_print:
